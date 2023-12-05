b8731ec9d6809ee172ffd909744fbaff2b6231c6
Spicylab <marc.killgour@gmail.com>
2023-12-05 12:23:48 +0100

    chore: testing build/publish

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { Button } from './ui/buttons/Button/Button'
+import { RoundButton } from './ui/buttons/RoundButton/RoundButton'
+
+export { Button, RoundButton }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
-import {vanillaExtractPlugin} from '@vanilla-extract/vite-plugin'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  build: {
+    outDir: 'lib',
+    emptyOutDir: true,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: 'src/main.ts',
+      name: 'azulgato-ui',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `azulgato-ui.${format}.js`,
+    },
+    sourcemap: true,
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+  },
 })
