5609fa9941d1e38ba0868516e808d6058be58b64
Spicylab <marc.killgour@gmail.com>
2023-12-05 13:04:29 +0100

    chore: exposing theme

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Button } from './ui/buttons/Button/Button'
 import { RoundButton } from './ui/buttons/RoundButton/RoundButton'
+import { ThemeProvider } from './theme/ThemeContext'
 
-export { Button, RoundButton }
+export { Button, RoundButton, ThemeProvider }
--- vite.config.ts
+++ vite.config.ts
@@ -25,4 +25,9 @@ export default defineConfig({
       },
     },
   },
+  css: {
+    modules: {
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
+    },
+  },
 })
