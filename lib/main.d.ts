3485d4cbf501f797f73445418e1d84f18afe410f
Spicylab <marc.killgour@gmail.com>
2024-01-26 16:44:39 +0100

    fix: export * as

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,6 +1,6 @@
 import { Button } from './ui/buttons/Button/Button';
 import { RoundButton } from './ui/buttons/RoundButton/RoundButton';
 import { ThemeProvider } from './theme/ThemeContext';
-import { Select } from './components/ui/select/select';
-import { Switch } from './components/ui/switch/switch';
+import * as Select from './components/ui/select/select';
+import * as Switch from './components/ui/switch/switch';
 export { Button, RoundButton, ThemeProvider, Select, Switch };
