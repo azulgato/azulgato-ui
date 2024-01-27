524c48bf2843d15e78d949fe47f9adfa38709b10
Spicylab <marc.killgour@gmail.com>
2024-01-27 20:44:07 +0100

    chore: TextInput 0.2.4

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -3,4 +3,5 @@ import { RoundButton } from './ui/buttons/RoundButton/RoundButton';
 import { ThemeProvider } from './theme/ThemeContext';
 import * as Select from './components/ui/select/select';
 import * as Switch from './components/ui/switch/switch';
-export { Button, RoundButton, ThemeProvider, Select, Switch };
+import { TextInput } from './ui/inputs/TextInput/TextInput';
+export { Button, RoundButton, ThemeProvider, Select, Switch, TextInput };
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { ThemeProvider } from './theme/ThemeContext'
 import * as Select from './components/ui/select/select'
 import * as Switch from './components/ui/switch/switch'
 import '../app/globals.css'
+import { TextInput } from './ui/inputs/TextInput/TextInput'
 
-export { Button, RoundButton, ThemeProvider, Select, Switch }
+export { Button, RoundButton, ThemeProvider, Select, Switch, TextInput }
